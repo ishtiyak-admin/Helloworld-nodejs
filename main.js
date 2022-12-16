641fd057b8c686d8b318518846c8b35ccc07fc43
yugandhar <yugandhar@devtechnosys.info>
2022-12-16 10:42:47 +0530

    update 16DEC2022

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end(' ***************************************my website is under maintenence it will be live soon please be  ');
+   response.end(' ***my website is under maintenence
+it's working now final testing pending by me  ');
 }).listen(8080);
 
 // Console will print the message
